9fc2ec21b003a5bec9299999cb509085965849bb
ultrasuperstick-tech <ultrasuperstick@gmail.com>
2026-07-08 22:09:28 +0900

    07/08 주석 달기 및 게임오버 시스템 구현
    01.내가 짠 스크립트들에게 주석을 달았다
    02.EndFlag라는 게임오브젝트를 만들어 닿으면 scene이 전환되는 시스템을 구현하였다

--- Assets/02.Scripts/AppleContorller.cs
+++ Assets/02.Scripts/AppleContorller.cs
@@ -1,33 +1,49 @@
+using Unity.VectorGraphics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AppleContorller : MonoBehaviour
 {
-    Rigidbody2D rbody;
-    float torquePower = 1.0f;
-    public float jumpPower = 1.0f;
+    Rigidbody2D rbody;             
+    float torquePower = 1.0f;   // 구르는 힘, 
+    public float jumpPower = 1.0f;  // 점프하는 힘.
+    public string sceneName;    // 바뀔 scene 이름.
 
     private void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
     }
 
+    // FInish 라는 테그을 가지고있는 다른 오브젝트와 충돌하면 scene을 sceneName(변수)로 전환한다.
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("FInish"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
     void FixedUpdate()
     {
+        // D키를 누르면 torquePower 에 -1을 곱한 값을 움직인다.
         if (Input.GetKey(KeyCode.D))
         {
             rbody.AddTorque(-torquePower);
         }
 
+        // A키를 누르면 torquePower 에 1을 곱한 값을 움직인다.
         if (Input.GetKey(KeyCode.A))
         {
             rbody.AddTorque(torquePower);
         }
 
+        // 이건 모름.
         if (Mathf.Abs(rbody.angularVelocity) > 300f)
         {
             rbody.angularVelocity = Mathf.Sign(rbody.angularVelocity) * 300f;
         }
 
+        // 스페이스 키를 누르고 linearVelocityY가 0 이라면 Vector2.up 에 jumpPower만큼 곱해 위로 힘을 준다.
         if (Input.GetKey(KeyCode.Space) && this.rbody.linearVelocityY == 0)
         {
             rbody.AddForce(Vector2.up * jumpPower);
--- Assets/02.Scripts/CameraController.cs
+++ Assets/02.Scripts/CameraController.cs
@@ -5,8 +5,13 @@ public class CameraController : MonoBehaviour
 
     private void LateUpdate()
     {
+        //이 오브젝트의 좌표를 얻는다
         Vector3 pos = this.transform.position;
+        
+        // z좌표를 -10으로 둔다
         pos.z = -10;
+        
+        //키메라가 이 오브젝트의 위치로 이동한다
         Camera.main.gameObject.transform.position = pos;
     }
 }
