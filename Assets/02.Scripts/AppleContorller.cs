f5d46856c2dde4aabf33e0515018aab2c020c59d
ultrasuperstick-tech <ultrasuperstick@gmail.com>
2026-07-09 21:03:49 +0900

    07/09 죽는 시스템
    01.죽는 시스템을 일정 좌표이상 떨어지면 sence를 초기화하여 죽는 기능을 추가해 보았다

--- Assets/02.Scripts/AppleContorller.cs
+++ Assets/02.Scripts/AppleContorller.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 public class AppleContorller : MonoBehaviour
 {
     Rigidbody2D rbody;             
-    float torquePower = 1.0f;   // 구르는 힘, 
+    public float torquePower = 1.0f;   // 구르는 힘, 
     public float jumpPower = 1.0f;  // 점프하는 힘.
     public string sceneName;    // 바뀔 scene 이름.
 
@@ -17,9 +17,17 @@ public class AppleContorller : MonoBehaviour
     // FInish 라는 테그을 가지고있는 다른 오브젝트와 충돌하면 scene을 sceneName(변수)로 전환한다.
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("FInish"))
+        if (other.CompareTag("Finish"))
         {
             SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    private void Update()
+    {
+        if (transform.position.y <= -2f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 
