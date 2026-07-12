6aa308e91fcaa3d4905973741bbb1f14e2cd585d
ultrasuperstick-tech <ultrasuperstick@gmail.com>
2026-07-12 22:12:10 +0900

    07/12 애벌레 움직임 구현 및 데미지 시스템 구현.
    01.에벌래 움직으는 기능 구현.
    02.체력이 다는 시스템 구현.

--- Assets/02.Scripts/Health.cs
+++ Assets/02.Scripts/Health.cs
@@ -1,18 +1,24 @@
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 //이 클래스는 HpBar를 조절하는 역할을 가지고 있습니다.
 public class Health : MonoBehaviour
 {
     public GameObject hpBar;
 
-    private void Update()
+    private void Start()
     {
-        
+
     }
 
-    void HpBarControl() 
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        // if (collision.gameObject == "Bug")
+        // 만약에 충돌체의 태그가 "Bug"와 같다면...
+        if (collision.gameObject.CompareTag("Bug"))
+        {
+            // 나의 Hp가 깎이는 일.
+            hpBar.GetComponent<Image>().fillAmount -= 0.1f;
+        }
     }
 }
---
+++ Assets/02.Scripts/WormController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rBody;
+    public GameObject wP_L;
+    public GameObject wP_R;
+    public float wormSpeed = 1f;
+    int wormDir = 1;
+
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    private void Update()
+    {
+        // 이번 프레임에 이동할 거리 = 방향 * 속도 * 시간보정.
+        this.transform.position += Vector3.right * wormDir * wormSpeed * Time.deltaTime;
+
+        if (this.transform.position.x < wP_L.transform.position.x)
+        {
+            wormDir = 1;
+            spriteRenderer.flipX = true;
+        }
+
+        if (this.transform.position.x > wP_R.transform.position.x)
+        {
+            wormDir = -1;
+            spriteRenderer.flipX = false;
+        }
+    }
+
+        //this.rBody.AddForce(Vector2.right * wormDir * wormSpeed);
+}
